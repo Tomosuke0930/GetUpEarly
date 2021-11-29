5a13eb08622e0c050ee92ed05d2e7b8147544619
Tomosuke0930 <84496536+Tomosuke0930@users.noreply.github.com>
2021-11-29 21:17:10 +0900

    fix: privateKey

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
 require('solidity-coverage');
+const ethers = require('ethers')
+let wallet = new ethers.Wallet.createRandom()
+let privateKey = wallet.privateKey
 
-const ALCHEMY_API_KEY = "";
+const ALCHEMY_API_KEY = "mGZ22nK18MOJbzlUYZT-rs4K7wcCaabP";
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- const ROPSTEN_PRIVATE_KEY = "";
+
+ //const ROPSTEN_PRIVATE_KEY = "";
  module.exports = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+      accounts: [privateKey]
     }
   }
 };
